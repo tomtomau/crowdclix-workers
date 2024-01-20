8d0ca8cd352c363a8c52489a618a86b1cfc111b8
Tom Newby <me@tomnewby.net>
2024-01-20 10:30:27 +1000

    Seems to deploy too /shrug

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 def handleRequest(request):
     print("hello world")
-    return __new__(Response('Python Worker hello world!', {
+    return __new__(Response('Python Worker hello world with update!', {
         'headers' : { 'content-type' : 'text/plain' }
     }))
 
