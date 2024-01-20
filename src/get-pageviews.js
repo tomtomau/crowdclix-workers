81bacc54d4ec2abd16286e98f71797c886a4f678
toru173 <toru173+github@gmail.com>
2024-01-20 19:14:24 +1000

    Return pageview, or zero if query is undefined

--- src/get-pageviews.js
+++ src/get-pageviews.js
@@ -1,11 +1,13 @@
 const DB = "crowdclix-data";
 
-export async function getArtist(env, artistID) {
+export async function getPageviews(env, artistID) {
     const {results} = await env.DB.prepare(
         "SELECT ArtistPageviews FROM Artists WHERE ArtistID = ?"
     )
         .bind(artistID)
         .all();
 
-    return Response.json(results);
+    // Return zero count if no pageviews
+    if (results[0] == null) return Response.json({"ArtistPageviews":0});
+    return Response.json(results[0]);
 }
