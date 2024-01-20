8b9de43351084a1493e7ac709ca58d38e249dbae
toru173 <toru173+github@gmail.com>
2024-01-20 16:28:06 +1000

    Syntax Error in Schema
    Syntax Error in Schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS Artists;
 CREATE TABLE IF NOT EXISTS Artists (ArtistID INTEGER PRIMARY KEY, ArtistName TEXT, ArtistDescription TEXT, ArtistLogoURL TEXT);
-INSERT INTO Customers (ArtistID, ArtistName, ArtistDescription, ArtistLogoURL) VALUES (1, 'Sonic Mirage', 'Sonic Mirage captivates audiences with ethereal melodies and hypnotic rhythms, blending genres seamlessly. Their music, a cosmic fusion of dreamy synth waves and dynamic beats, creates an immersive sonic journey.', 'https://i.ytimg.com/vi/1yMwfWDG294/maxresdefault.jpg'));
+INSERT INTO Customers (ArtistID, ArtistName, ArtistDescription, ArtistLogoURL) VALUES (1, 'Sonic Mirage', 'Sonic Mirage captivates audiences with ethereal melodies and hypnotic rhythms, blending genres seamlessly. Their music, a cosmic fusion of dreamy synth waves and dynamic beats, creates an immersive sonic journey.', 'https://i.ytimg.com/vi/1yMwfWDG294/maxresdefault.jpg');
