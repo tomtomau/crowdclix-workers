ccc34e01fa8a7c1ddd37401bd231f851f0862f70
toru173 <toru173+github@gmail.com>
2024-01-21 14:49:26 +1000

    Correct DB Query

--- src/get-signups.js
+++ src/get-signups.js
@@ -2,7 +2,7 @@ export async function getSignups(env, DB, headers, artistID) {
     // Grabs all signups for a given ArtistID and returns them ordered
     // oldest first
     const signups = await env.DB.prepare(
-        "SELECT FanEmails FROM Signups WHERE ArtistID = ?"
+        "SELECT FanEmail FROM Signups WHERE ArtistID = ?"
     )
         .bind(artistID)
         .all();
