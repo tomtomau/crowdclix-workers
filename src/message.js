b04580c4e25a714327f83ba53e513fab5958d13d
G4rv19 <garvsidana29@gmail.com>
2024-01-21 14:05:39 +1000

    handle the verification of the artist id

--- src/message.js
+++ src/message.js
@@ -5,7 +5,7 @@ export async function postmessages(env, body, id) {
     var requestBody = JSON.parse(stream_string);
     var messageString = requestBody.message;
 
-    const artist = await env.DB.prepare(
+    const artist = await env.DB.prepare(    // for verifying the artist id
         "SELECT * FROM Artists WHERE ArtistID = ?"
     )
         .bind(id)
@@ -13,9 +13,18 @@ export async function postmessages(env, body, id) {
 
         if (artist === null) {
             throw new Error('Not found');
-        }
+        } 
 
-    
+    const { success } = await env.DB.prepare(
+        //"INSERT INTO Signups (ArtistID, CustomerEmail) VALUES (?, ?)" //Test query
+        "INSERT INTO Messages (ArtistID, MessageContents) VALUES (?, ?)" //Final query
+    )
+        .bind(id, messageString) // This binds values to the question nmarks in the query.
+        .run();
 
-
+    if (success) {
+        console.log("message got stored.");
+        return new Response(null, { status: 200 })        
+    }
+    return new Response(null, { status: 404 })
 }
