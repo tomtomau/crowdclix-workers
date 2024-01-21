c7e1e40e7150a7af0b2b8c0c82307c366dd2749a
Tom Newby <me@tomnewby.net>
2024-01-21 14:13:08 +1000

    fix(cors): fix more cors

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import {getMessages} from "./get-messages";
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-const hardcodedArtistName    = "Sonic Mirage"
+const hardcodedArtistName = "Sonic Mirage"
 const DB = "crowdclix-data";
 
 
@@ -23,35 +23,39 @@ export default {
             ['Access-Control-Allow-Origin', '*']
         ];
 
-      const {pathname} = new URL(request.url);
-      const method = request.method;
-      const body = request.body;
+        const {pathname} = new URL(request.url);
+        const method = request.method;
+        const body = request.body;
 
-      if (pathname.startsWith('/artists')) {
-        const id = pathname.split('/').pop()
-        return getArtist(env, DB, headers, id);
-      }
+        if (method === 'OPTIONS') {
+            return new Response(null, {status: 200, headers: headers});
+        }
 
-      if (pathname.startsWith('/pageviews') && method === "GET") {
-        const id = pathname.split('/').pop()
-        return getPageviews(env, DB, headers, id);
-      }
+        if (pathname.startsWith('/artists')) {
+            const id = pathname.split('/').pop()
+            return getArtist(env, DB, headers, id);
+        }
 
-      if (pathname.startsWith('/pageviews') && method === "POST") {
-        const id = pathname.split('/').pop()
-        return postPageviews(env, DB, id);
-      }
+        if (pathname.startsWith('/pageviews') && method === "GET") {
+            const id = pathname.split('/').pop()
+            return getPageviews(env, DB, headers, id);
+        }
 
-      if (pathname.startsWith('/signup') && method === "POST") {
-        const id = pathname.split('/').pop()
-        //console.log(request);
-        return postSignup(env, body, headers, id);
-      }
+        if (pathname.startsWith('/pageviews') && method === "POST") {
+            const id = pathname.split('/').pop()
+            return postPageviews(env, DB, id);
+        }
 
-      if (pathname.startsWith('/messages') && method === "GET") {
-        const id = pathname.split('/').pop()
-        return getMessages(env, DB, headers, id);
-      }
-      return new Response("Error Not Found")
+        if (pathname.startsWith('/signup') && method === "POST") {
+            const id = pathname.split('/').pop()
+            //console.log(request);
+            return postSignup(env, body, headers, id);
+        }
+
+        if (pathname.startsWith('/messages') && method === "GET") {
+            const id = pathname.split('/').pop()
+            return getMessages(env, DB, headers, id);
+        }
+        return new Response("Error Not Found")
     }
 };
